9c6e9f5c6f98302d9c209e59de3ce46388614a9b
Rasmus Munksø <ramu@ihfood.dk>
2024-05-13 10:10:59 +0200

    More script conversion for pipelines

--- tutorial_train_wandb.py
+++ tutorial_train_wandb.py
@@ -2,7 +2,7 @@ from share import *
 
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
-from tutorial_dataset import MyDataset
+from dataset import MyDataset
 from cldm.logger import ImageLogger
 from cldm.model import create_model, load_state_dict
 
